d536407cf67707e13e55a59bc5f81a65c60122d4
Al <Alexandre.maniquant@gmail.com>
2021-03-11 03:04:25 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas
+import pandas_datareader.data as web # Package et modules pour importer les datas
+import datetime
+
+startdate = datetime.date(2018,1,1)
+mydate = datetime.date.today()
+
+print(mydate.year)

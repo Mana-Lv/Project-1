f79cbe237d3040133c90e3c2096c3cc70cfbd112
Al <Alexandre.maniquant@gmail.com>
2021-03-24 01:31:42 +0100

    [Evol] Improvement of the general appearance (multiplechart)

--- multipleplot_chart.py
+++ multipleplot_chart.py
@@ -2,15 +2,20 @@ import pandas as pd
 import quandl
 import datetime
 import numpy as np
+
 from log_quandl import *
 
-### Log with quandl API key
+import matplotlib.pyplot as plt
+
+from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY
+
+# Log with quandl API key
 
 log_quandl()
 
-## Choose the date interval
+# Choose the date interval
 
-startdate = datetime.date(2021,3,15)
+startdate = datetime.date(2021,1,1)
 mydate = datetime.date.today()
 
 # Get the stock data for choosen companies
@@ -26,19 +31,52 @@ import re
 codeQuandl = ["EURONEXT/GLE", "EURONEXT/BNP", "EURONEXT/ACA"]
 
 stocks = list(map(getQuandl, codeQuandl))
-stocks_df = pd.DataFrame()
+print(stocks)
+print(type(stocks))
+print(type(stocks[1]))
 
 fields = ["Open", "High", "Low", "Last"]
+df = pd.DataFrame(index = stocks[1].index) # Empty Dataframe with the index of the stocks
 
-a = stocks[1].loc[:,fields]
+title = "Cours "
 
-# Test 
-for i in range(len(codeQuandl)):
-    stocks[i].loc[:,fields]
-    a = re.search("/", codeQuandl[i])
-    b = codeQuandl[i][a.span()[1]:len(codeQuandl[1])]
-    stocks_df.append({b : stocks[1]["Open"]}, ignore_index=True)
-    c = stocks_df.append({b : stocks[1]["Open"]}, ignore_index=True)
-    print(c)
+# Test on dataframe
+for i in range(len(stocks)):
+    stocks[i] = stocks[i].loc[:,fields]
+    crit_ticker = re.search("/", codeQuandl[i])
+    ticker = codeQuandl[i][crit_ticker.span()[1]:len(codeQuandl[i])]
+    df[ticker] = stocks[i]["Open"]
+    title = title + ticker + " "
 
+#http://python-simple.com/python-matplotlib/configuration-axes.php
 
+plt.style.use("seaborn-dark")
+
+fig, ax = plt.subplots()
+
+fig.autofmt_xdate() # Rotate the legend automatically to have enought space
+
+if df.index[-1] - df.index[0] < pd.Timedelta("730 days"):
+    weekFormatter = DateFormatter('%b %d')
+    mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
+    alldays = DayLocator()                  # minor ticks on the days
+
+    ax.xaxis.set_major_locator(mondays)
+    ax.xaxis.set_minor_locator(alldays)
+
+else:
+    
+    weekFormatter = DateFormatter('%b %d %Y')
+
+ax.xaxis.set_major_formatter(weekFormatter)
+
+plt.grid(True)
+
+plt.title(title)
+plt.plot(df) # df.plot work but for now this one works better
+
+print(plt.style.available)
+
+plt.tight_layout()
+
+plt.show()
